03e116cdc0397ee4a640bb5337bc4c5dc44e8adb
iamfet <fetuyee@gmail.com>
2025-06-26 21:09:54 +0000

    feat(backend): add backend configuration

--- backend/main.tf
+++ backend/main.tf
@@ -1,40 +1,62 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.9"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "state-eks-deploy"
 
-# provider "aws" {
-#   region = "us-east-1"
-# }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "state-eks-secure-infra-automation"
-#
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "terraform_state" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  policy = jsonencode({
+    Statement = [{
+      Effect    = "Deny"
+      Principal = "*"
+      Action    = "s3:*"
+      Resource  = ["${aws_s3_bucket.terraform_state.arn}/*"]
+      Condition = {
+        Bool = { "aws:SecureTransport" = "false" }
+      }
+    }]
+  })
+}
+
+
 
 # /*resource "aws_dynamodb_table" "terraform_locks" {
 #   name         = "terraform-eks-state-locks"
